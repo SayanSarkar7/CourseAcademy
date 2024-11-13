ad5be6eb9e7a7e23f8a6c27ffa7d8c923e638046
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-11-13 12:07:01 +0530

    updated profile page

--- src/components/Profile/ChangePassword.jsx
+++ src/components/Profile/ChangePassword.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const ChangePassword = () => {
+  const [oldPassword,setOldPassword]=useState('');
+  const [newPassword,setNewPassword]=useState('');
   return (
-    <div>ChangePassword</div>
+    <Container py={'16'} minH={'90vh'}>
+        <form>
+          <Heading children={'Change Password'} my={'16'} textAlign={['center','left']} textTransform={'uppercase'} />
+          <VStack spacing={'8'}>
+          <Input
+              required
+              id="password"
+              value={oldPassword}
+              onChange={e => {
+                setOldPassword(e.target.value);
+              }}
+              placeholder={'Enter Old Password'}
+              type={'password'}
+              focusBorderColor={'yellow.500'}
+            />
+          <Input
+              required
+              id="password"
+              value={newPassword}
+              onChange={e => {
+                setNewPassword(e.target.value);
+              }}
+              placeholder={'Enter New Password'}
+              type={'password'}
+              focusBorderColor={'yellow.500'}
+            />
+            <Button type={'submit'} w={'full'} colorScheme={'yellow'} >Change</Button>
+          </VStack>
+        </form>
+    </Container>
   )
 }
 
--- src/components/Profile/UpdateProfile.jsx
+++ src/components/Profile/UpdateProfile.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 const UpdateProfile = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   return (
-    <div>UpdateProfile</div>
-  )
-}
+    <Container py={'16'} minH={'90vh'}>
+      <form>
+        <Heading
+          children={'Change Password'}
+          my={'16'}
+          textAlign={['center', 'left']}
+          textTransform={'uppercase'}
+        />
+        <VStack spacing={'8'}>
+          <Input
+            value={name}
+            onChange={e => {
+              setName(e.target.value);
+            }}
+            placeholder={'Name'}
+            type={'text'}
+            focusBorderColor={'yellow.500'}
+          />
+          <Input
+            value={email}
+            onChange={e => {
+              setName(e.target.value);
+            }}
+            placeholder={'Email'}
+            type={'email'}
+            focusBorderColor={'yellow.500'}
+          />
 
-export default UpdateProfile
+          <Button type={'submit'} w={'full'} colorScheme={'yellow'}>
+            Update
+          </Button>
+        </VStack>
+      </form>
+    </Container>
+  );
+};
+
+export default UpdateProfile;
